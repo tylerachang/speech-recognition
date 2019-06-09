a70519b28e57c6196922af9dc36061328b10e78a
tylerachang98 <changt@cmc304-10.mathcs.carleton.edu>
2019-06-09 14:22:37 -0500

    Removed extra files and pushed datasets

--- main.py
+++
@@ -1,8 +0,0 @@
-"""
-	Recognizes audio files (.wav format) using neural networks.
-"""
-
-import neuralNetworks as nn
-
-neuralNetwork = nn.NeuralNetwork(5, 500, 100, [100,100,100])
-neuralNetwork.train_neural_network(10)
